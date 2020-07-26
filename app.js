69586ead5d101911419478f696ff3b85e7635cd4
Barun Ghosh <barunonline678@gmail.com>
2020-07-26 11:10:31 +0530

    Stupid way of finding square root 🤺

---
+++ app.js
@@ -0,0 +1,51 @@
+// Hare Krishna
+// Finding factors
+function getAllFactors(n) {
+  let factors = [];
+
+  for (let i = 2; i <= n; i++) {
+    while ((n % i) === 0) {
+      factors.push(i);
+      n /= i;
+    }
+  }
+
+  return factors;
+}
+
+// Finding ocurrences of factors
+const countOccurrences = (arr) => {
+  const map = {};
+  for (var i = 0; i < arr.length; i++) {
+    map[arr[i]] = ~~map[arr[i]] + 1;
+  }
+  return map;
+};
+
+// Sum a, b times
+function calc(a, b) {
+  sum = 0;
+  for (let i = 1; i <= b / 2; i++) {
+    sum += a;
+  }
+  return sum;
+}
+
+// Check if number is even
+const isEven = (v) => v % 2 === 0;
+
+// Calculating root by multiplying sum of occurences of factors
+function getRoot(n) {
+  let root = 1;
+  let occur = countOccurrences(getAllFactors(n));
+  if (Object.values(occur).every(isEven)) {
+    for (const [key, value] of Object.entries(occur)) {
+      root = root * calc(parseInt(key), parseInt(value));
+    }
+  }
+    return root;
+}
+
+console.log(getRoot(529));
+
+
